336b2bc60c74d226ea6057fbce8842fbaacd593b
iamxadnan <adnanshariffpc@gmail.com>
2025-06-22 10:30:17 +0530

    modified:   Week 1/2-Algorithms_Data Structures/Code/ECommersPlatformSearch/src/ecommers/BinarySearch.java

--- Week 1/2-Algorithms_Data Structures/Code/ECommersPlatformSearch/src/ecommers/BinarySearch.java
+++ Week 1/2-Algorithms_Data Structures/Code/ECommersPlatformSearch/src/ecommers/BinarySearch.java
@@ -3,24 +3,24 @@ import java.util.*;
 public class BinarySearch {
     public static void main(String[] args){
         Product[] p= new Product[5];
-        p[0]= new Product(1,"rolex","watches");
-        p[1]= new Product(2,"infinix","mobile");
-        p[2]= new Product(3,"pavillion","laptop");
-        p[3]= new Product(4,"titan","watches");
-        p[4]= new Product(5,"oppo buds","accessories");
+        p[0]= new Product(17,"rolex","watches");
+        p[1]= new Product(20,"infinix","mobile");
+        p[2]= new Product(39,"pavillion","laptop");
+        p[3]= new Product(41,"titan","watches");
+        p[4]= new Product(50,"oppo buds","accessories");
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
         int low=0;
         int high=p.length-1;
         int mid=(low+high)/2;
         int count=0;
-        for(int i=0;i<mid;i++){
+        for(int i=0;low<=high;i++){
             if(p[mid].ProductId==n){
                 System.out.println("Product is found at index:"+mid);
                 count=1;
                 break;
             }
-            else if(n>mid){
+            else if(n>p[mid].ProductId){
                 low=mid+1;
                 mid=(low+high)/2;
 
