f806655aafcb0e49a3eb86ea1b06a39272c7abaf
iamxadnan <adnanshariffpc@gmail.com>
2025-07-06 15:54:57 +0530

    modified:   Week 3/1-Spring Core and Maven/LibraryManagement/pom.xml

---
+++ Week 3/1-Spring Core and Maven/LibraryManagement/src/main/java/com/nurture/LibraryManagement/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.nurture.LibraryManagement.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+    public void saveBook(String bookName) {
+        System.out.println("Book '" + bookName + "' saved to the repository.");
+    }
+}
