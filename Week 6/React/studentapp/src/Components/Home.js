6ec4f7cd6050cc4f6a7b6de34b5a00731264e040
iamxadnan <adnanshariffpc@gmail.com>
2025-07-27 12:05:54 +0530

    new file:   Week 6/React/output/exercise 2.png

---
+++ Week 6/React/studentapp/src/Components/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div><h3>Welcome to Home page of student management Portal</h3></div>
+    )
+  }
+}
